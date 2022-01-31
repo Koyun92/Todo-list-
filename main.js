b0fcc19f9cab1b5f0f70dc6d56ef1e28288cacbd
Borys <borys.baca@gmail.com>
2022-01-31 11:36:57 +0100

    remove item function completed

--- main.js
+++ main.js
@@ -1,63 +1,73 @@
 // Global Variables
 const input = document.querySelector('.todo__input');
-const crosses = document.getElementsByClassName('todo__cross')
 
+const todoList = document.querySelector('.todo__list')
 const circle = document.querySelectorAll('.todo__circle');
-let todoID = 0;
 let listItems = [];
-let todoStatus = document.querySelector('.todo__status span')
+const todoStatus = document.querySelector('.todo__status span')
+const clearBtn = document.querySelector('.todo__btn-clear')
 
 // Functions
 
 const addTodo = () => {
     const li = document.createElement('li');
     li.classList.add('todo__list-item', 'todo__box')
-    li.setAttribute("data-id", todoID)
+
     li.innerHTML = `<div class="todo__circle"> </div>
     <p>${input.value}
     </p>
     <div class="todo__box--cross"><img class="todo__cross" src="images/icon-cross.svg" alt=""></div>`
-    listItems.push(li)
+    listItems.push(li);
     input.value = "";
-    todoID++
+    renderTodos();
 
 }
 
 const renderTodos = () => {
-    const todoList = document.querySelector('.todo__list')
+    todoList.innerHTML = "";
 
-    listItems.forEach(item => {
+    listItems.forEach((item, index) => {
         todoList.appendChild(item);
-
-
+        item.setAttribute("data-id", index);
     })
     todoStatus.innerHTML = listItems.length + " "
 
 }
-const removeTask = () => {
-    const crosses = document.querySelectorAll('.todo__cross')
-    crosses.forEach(element => {
-        element.addEventListener('click', function () {
-
-            console.log(this.parentNode.parentNode.dataset.id);
-            listItems.splice(this.parentNode.parentNode.dataset.id, 1)
-
-        })
-    });
+const removeTodo = (e) => {
+    console.log(e.target.closest('li').dataset.id);
+    listItems.splice(e.target.closest('li').dataset.id, 1);
+    renderTodos();
+}
+const checkClick = (e) => {
+    if (e.target.matches(".todo__cross")) {
+        removeTodo(e);
+    } else if (e.target.matches('.todo__circle')) {
+        e.target.classList.toggle('checked')
+    }
 }
 
 
 
 
 
-// Event Listeners
+// // Event Listeners
+// INPUT
 input.addEventListener('keyup', (e) => {
     if (e.keyCode === 13 && input.value) {
         addTodo();
-        renderTodos();
-
     }
 })
 
+// TODOLIST 
+todoList.addEventListener('click', ((e) => {
+    checkClick(e);
+}))
 
-// Ustawic usuwanie z tablicy i renderowanie po działaniu
+
+clearBtn.addEventListener('click', () => {
+    listItems = [];
+    renderTodos();
+})
+
+
+// ustawić Checked do końca w elemencie after >> zrobić clear completed>> zrobić filtrowanie>> zrobić drag and drop>> zrobić zmianę kolorów.
