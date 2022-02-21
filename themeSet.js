83112a6105be0e6b7151ca160024720f45470bbc
Borys <borys.baca@gmail.com>
2022-02-21 23:19:01 +0100

    src correct

--- themeSet.js
+++ themeSet.js
@@ -20,8 +20,8 @@ const themeSwitch = () => {
         root.style.setProperty('--font-color', 'hsl(234, 39%, 85%)');
         root.style.setProperty('--background-box', ' #25273c');
         root.style.setProperty('--background-body-color', 'hsl(235, 21%, 11%)');
-        root.style.setProperty('--background-image-mobile', 'url(/images/bg-mobile-dark.jpg)');
-        root.style.setProperty('--background-image-desktop', 'url(/images/bg-desktop-dark.jpg)')
+        root.style.setProperty('--background-image-mobile', 'url(../images/bg-mobile-dark.jpg)');
+        root.style.setProperty('--background-image-desktop', 'url(../images/bg-desktop-dark.jpg)')
 
 
         darkMode = !darkMode;
@@ -33,8 +33,8 @@ const themeSwitch = () => {
         root.style.setProperty('--font-color', 'hsl(234, 11%, 52%)');
         root.style.setProperty('--background-box', ' #fff');
         root.style.setProperty('--background-body-color', 'hsl(0, 0%, 98%)')
-        root.style.setProperty('--background-image-mobile', 'url(/images/bg-mobile-light.jpg)')
-        root.style.setProperty('--background-image-desktop', 'url(/images/bg-desktop-light.jpg)')
+        root.style.setProperty('--background-image-mobile', 'url(../images/bg-mobile-light.jpg)')
+        root.style.setProperty('--background-image-desktop', 'url(../images/bg-desktop-light.jpg)')
 
     }
     console.log('root setted');
