6e187d43831461acbf9999ec0878b865969e7242
Borys <borys.baca@gmail.com>
2022-02-21 22:54:51 +0100

    theme mode color adjusting

--- themeSet.js
+++ themeSet.js
@@ -10,13 +10,32 @@
 const root = document.documentElement;
 const themeBtn = document.querySelectorAll('.todo__icon')
 let darkMode = true;
+const icon = document.querySelector('.todo__header .todo__icon')
 const themeSwitch = () => {
 
     if (darkMode) {
+        icon.setAttribute('src', '/images/icon-sun.svg')
+
         root.style.setProperty('--font-color-hover', 'hsl(236, 33%, 92%)');
+        root.style.setProperty('--font-color', 'hsl(234, 39%, 85%)');
         root.style.setProperty('--background-box', ' #25273c');
         root.style.setProperty('--background-body-color', 'hsl(235, 21%, 11%)')
-        root.style.setProperty('--background-image', 'url(/images/bg-mobile-dark.jpg)')
+        root.style.setProperty('--background-image-mobile', 'url(/images/bg-mobile-dark.jpg)')
+        root.style.setProperty('--background-image-desktop', 'url(/images/bg-desktop-dark.jpg)')
+
+
+        darkMode = !darkMode;
+
+    } else {
+        darkMode = !darkMode;
+        icon.setAttribute('src', '/images/icon-moon.svg')
+        root.style.setProperty('--font-color-hover', 'hsl(235, 19%, 35%)');
+        root.style.setProperty('--font-color', 'hsl(234, 11%, 52%)');
+        root.style.setProperty('--background-box', ' #fff');
+        root.style.setProperty('--background-body-color', 'hsl(0, 0%, 98%)')
+        root.style.setProperty('--background-image-mobile', 'url(/images/bg-mobile-light.jpg)')
+        root.style.setProperty('--background-image-desktop', 'url(/images/bg-desktop-light.jpg)')
+
     }
     console.log('root setted');
     console.log('switch theme ');
